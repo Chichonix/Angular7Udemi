80db4bded2aab7a50f7182b36f3e0add41ede7ac
Chichonix <chicho.dan45@gmail.com>
2019-01-03 13:32:48 +0100

    Populate Database

---
+++ full-stack-with-angular-and-spring-boot/backend/service/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into todo(id, username, description, target_date, is_done)
+values(100001, 'chicho', 'Learn JPA', sysdate , false);
+
+insert into todo(id, username, description, target_date, is_done)
+values(100002, 'chicho', 'Learn Dara JPA', sysdate , false);
+
+insert into todo(id, username, description, target_date, is_done)
+values(100003, 'chicho', 'Learn MicroServices', sysdate , false);
