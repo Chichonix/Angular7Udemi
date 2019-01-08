43c4eb3bc9e26eed95ef89f9b7fc2df489aef047
Chichonix <chicho.dan45@gmail.com>
2019-01-08 16:58:43 +0100

    End of JPA Apendix

---
+++ full-stack-with-angular-and-spring-boot/jpa/jpain10steps/src/main/java/com/in28minutes/learning/jpa/jpain10steps/entity/User.java
@@ -0,0 +1,24 @@
+package com.in28minutes.learning.jpa.jpain10steps.entity;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+// Table - User
+@Entity
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue
+    private long id;
+    private String name;
+    private String role;
+
+    public User(String name, String role) {
+        this.name = name;
+        this.role = role;
+    }
+}
